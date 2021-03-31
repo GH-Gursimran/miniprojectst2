667009e95f9a5fdf43f45313494d7b6ece517bf8
GH-Gursirman <sohanpalgursimransingh@gmail.com>
2021-03-31 18:01:05 +0530

    basic

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.Date;
-
 @Controller
 public class HomeController {
 
