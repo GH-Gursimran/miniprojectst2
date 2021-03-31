f9c203c7e32f0d31bf05c3216464b0beeb8e9bb8
GH-Gursirman <sohanpalgursimransingh@gmail.com>
2021-03-31 17:32:56 +0530

    initial

---
+++ src/main/java/com/backend/miniprojectst2/Miniprojectst2Application.java
@@ -0,0 +1,13 @@
+package com.backend.miniprojectst2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+@SpringBootApplication
+public class Miniprojectst2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Miniprojectst2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/miniprojectst2/Miniprojectst2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.miniprojectst2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miniprojectst2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
